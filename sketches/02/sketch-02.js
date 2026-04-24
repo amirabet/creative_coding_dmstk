c1313b8ae657e8a814626a55f549bd46dd1fe7f9
Artur Mirabet <arturmirabet@gmail.com>
2026-04-24 11:24:56 +0200

    wip: 03E arcs

--- sketches/02/sketch-02.js
+++ sketches/02/sketch-02.js
@@ -5,17 +5,22 @@ const settings = {
   dimensions: [1080, 1080],
 };
 //
+// Colors
+const color_background = "black";
+const color_elements = "white";
+const color_highlight = "red";
+//
 // Utility functions
 // Not used since we are using now canvas-skecth-util
 // const degToRad = (angle) => (angle * Math.PI) / 180;
 // const randomRange = (min, max) => Math.random() * (max - min) + min;
-
+//
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = color_background;
     context.fillRect(0, 0, width, height);
     // The square to distort
-    context.fillStyle = "black";
+    context.fillStyle = color_elements;
     const cx = width * 0.5;
     const cy = height * 0.5;
     // const w = width * 0.3;
@@ -26,7 +31,7 @@ const sketch = () => {
     const h = height * 0.1;
     //
     // Make now the hours indicators
-    const slices = 12;
+    const slices = 36;
     const slice = math.degToRad(360 / slices);
     //
     // Using trigonometry of place each hour indicator around the external circle
@@ -58,15 +63,35 @@ const sketch = () => {
       // since we're restoring the context below
       //context.scale(Math.max(0.4, Math.random()) * 3, 1);
       // Using helper function
-      context.scale(random.range(1, 3), 1);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.8));
       //
       // Paint the square
       context.beginPath();
       // Paint the square accounting the translate
       // and put the center of the square in the middle of the rotation
-      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
       context.fill();
       // Recover the prevoius canvas state (before translate)
+      context.restore();
+      //
+      // Decorational arcs
+      // Recover canvas position
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+      // Paint the arcs
+      context.lineWidth = random.range(5, 20);
+      context.strokeStyle = color_elements;
+      context.beginPath();
+      context.arc(
+        0,
+        0,
+        radius * random.range(0.7, 1.3),
+        slice * random.range(1, -8),
+        slice * random.range(1, 5),
+      );
+      context.stroke();
+
       context.restore();
     }
     //
